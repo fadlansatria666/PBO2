18c343f5cfe36071cf8f2dd050c245b72895a805
fadlansatria666 <127916796+fadlansatria666@users.noreply.github.com>
2023-06-03 12:48:22 +0700

    Add files via upload

---
+++ Praktikum 3/overload1.py
@@ -0,0 +1,6 @@
+# Nama  : Fadlan Satria Triswanto
+# Kelas : R1
+# NIM   : 210510001
+
+print(len("geeks"))
+print(len([10, 20, 30]))
---
+++ Praktikum 3/overload2.py
@@ -0,0 +1,7 @@
+# Nama  : Fadlan Satria Triswanto
+# Kelas : R1
+# NIM   : 210510001
+
+print(5 - 1)
+print("Above" + " The" + " Clouds")
+print([1, 2] + [3, 4]) 
